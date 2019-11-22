018e592301bc515ad1024e33d71d3f5ae2908c38
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-11-22 10:08:31 +0100

    Create RoomColumn.vue

---
+++ src/components/RoomColumn.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-col>
+      <b-row v-for="hour in hoursInDay" :key="hour.id" :room="room">{{ hour }} </b-row>
+  </b-col>
+</template>
+
+<script>
+export default {
+    name: "RoomColumn",
+     data() {
+    return {
+      hoursInDay: [],
+      rooms: ["#", "Room 1", "Room 2", "Room 3", "Room 4", ]
+    };
+  },
+  mounted() {
+    this.incrementHours();
+  },
+  methods: {
+    incrementHours() {
+      var quarterHours = ["00", "30"];
+      var times = [];
+      for (var i = 0; i < 24; i++) {
+        for (var j = 0; j < 2; j++) {
+          var time = i + ":" + quarterHours[j];
+          if (i < 10) {
+            time = "0" + time;
+          }
+          times.push(time);
+        }
+      }
+      this.hoursInDay = times;
+    },
+    selectedTime(room) {
+      console.log(room)
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
